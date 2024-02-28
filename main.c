80c8d01d90e905a440a931848f41aae235daad0a
Sojib Ahmed <150809073+sojib-ahmeds@users.noreply.github.com>
2024-02-28 12:04:59 +0600

    C Programming Multiplication Table

--- main.c
+++ main.c
@@ -1,16 +1,11 @@
 #include<stdio.h>
 int main(){
-int i,num1,num2,gcd;
-printf("Enter Your two Number :");
-scanf("%d %d",&num1,&num2);
-
-for( i = 1; i <= num1 && i <= num2; i++){
-    if(num1 % i == 0 && num2 % i == 0){
-        gcd = i;
-    }
+int number,i;
+printf("Enter Your number :");
+scanf("%d",&number);
+for(i=1;i<=10;i++){
+    printf("%d * %d = %d\n",number,i,number*i);
 }
- printf("GCD is : %d",gcd);
-
 
 return 0;
 }
