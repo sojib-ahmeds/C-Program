c5aecd6f761544a44cb6d9ef583d859e5af6f4d7
Sojib Ahmed <150809073+sojib-ahmeds@users.noreply.github.com>
2024-02-29 20:45:39 +0600

    Check Leap Year

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 int main(){
-int number;
-printf("Enter your Number :");
-scanf("%d",&number);
-
-if(number % 2 ==0){
-    printf("%d Number Is Even ",number);
+int year;
+printf("Enter Your year :");
+scanf("%d",&year);
+if(year % 400 == 0){
+    printf("%d is leep Year. ",year);
 }
-else{
-     printf("%d Number Is Odd",number);
+else if(year % 100 ==0){
+    printf("%d is not leep Year. ",year);
 }
-
+else if(year % 4 == 0){
+    printf("%d is leep Year. ",year);
+}
+else {
+    printf("%d is not leep Year. ",year);
+}
 return 0;
 }
